3cb6905be40ab4e7cbd57a40fb4a7e3fa74d60ac
alistuff <alistuff@163.com>
2015-05-14 23:48:34 +0800

    update

---
+++ src/game.js
@@ -0,0 +1,152 @@
+﻿//
+var Game = function (canvasid) {
+    self = this;
+    var canvas = document.getElementById(canvasid);
+    this.context = canvas.getContext("2d");
+
+    this.startTime = 0;
+    this.lastTime = 0;
+    this.gameTime = 0;
+    this.fps = 0;
+
+    this.contentRoot = '';
+    this.textures = {};
+
+    this.keyboardListeners = [];
+    window.onkeydown = function (e) { self.keyPressed(e); };
+  //  window.onkeypress = function (e) { self.keyPressed(e); };
+
+    return this;
+}
+
+Game.prototype = {
+
+    //get current time
+    getCurrentTime: function () {
+        return +new Date();
+    },
+
+    getFps: function () {
+        return this.fps;
+    },
+
+    windowToCanvas: function (x, y) {
+        var bounding = this.context.canvas.getBoundingClientRect();
+
+        return {
+            x: x - bounding.left * (this.context.canvas.width / bounding.width),
+            y: y - bounding.top * (this.context.canvas.height / bounding.height)
+        };
+    },
+
+    //start the game loop
+    start: function () {
+        var self = this;
+        this.startTime = this.getCurrentTime();
+        this.init(this.context);
+
+        window.requestNextAnimationFrame(function (time) {
+            self.animate.call(self, time);
+        });
+    },
+
+    //game animate
+    animate: function (time) {
+        var self = this;
+
+        if (this.lastTime == 0)
+            this.fps = 60;
+        else
+            this.fps = 1000 / (time - this.lastTime);
+
+        this.gameTime = this.getCurrentTime() - this.startTime;
+        this.lastTime = time;
+
+        this.update(time);
+        this.clearCanvas();
+        this.render(this.context);
+
+        window.requestNextAnimationFrame(function (time) {
+            self.animate.call(self, time);
+        });
+    },
+
+    //clear canvas
+    clearCanvas: function () {
+        this.context.clearRect(0, 0,
+            this.context.canvas.width, this.context.canvas.height);
+    },
+
+    //load texture
+    loadTexture: function (textureUrl) {
+        var image = new Image();
+        image.src = this.contentRoot + '/' + textureUrl;
+        self = this;
+
+        //image.addEventListener('load', function (e) {
+        //    self.textureLoadEvent(e);
+        //});
+
+        image.addEventListener('error', function (e) {
+            self.textureErrorEvent(textureUrl, e);
+        });
+
+        this.textures[textureUrl] = image;
+    },
+
+    //get texture
+    getTexture: function (textureUrl) {
+        return this.textures[textureUrl];
+    },
+
+    //delegates
+    init: function (context) { },
+    update: function (time) { },
+    render: function (context) { },
+
+    //events
+    textureLoadedEvent: function (e) { },
+    textureErrorEvent: function (url, e) { alert(url + e); },
+
+    //input
+
+    addMouseListener: function (type, listener) {
+        this.context.canvas.addEventListener(type, listener, false);
+    },
+
+    addKeyListener: function (key,listener) {
+        this.keyboardListeners.push({key:key,listener:listener });
+    },
+
+    keyPressed: function (e) {
+        var key = undefined;
+        var listener = undefined;
+
+        switch (e.keyCode) {
+            case 87: key = 'w'; break; 
+            case 65: key = 'a'; break; 
+            case 83: key = 's'; break; 
+            case 68: key = 'd'; break;
+            case 90: key = 'z'; break;
+            case 88: key = 'x'; break;
+            case 74: key = 'j'; break;
+            case 75: key = 'k'; break;
+            case 82: key = 'r'; break;
+            case 37: key = 'left'; break;
+            case 39: key = 'right'; break;
+            case 38: key = 'up'; break;
+            case 40: key = 'down'; break;
+            case 32: key = 'space'; break;
+            case 13: key = 'enter'; break;
+        }
+
+        for (var i = 0; i < this.keyboardListeners.length; i++) {
+            var pair = this.keyboardListeners[i];
+            if (pair.key === key && pair.listener) {
+                pair.listener();
+                window.returnValue = false;
+                break;
+            }
+        }
+    },
+};
---
+++ src/main.js
@@ -0,0 +1,73 @@
+﻿//
+//2d grid map ray-casting renderer version1.0
+//programming by hwl 2015/5/11
+//
+
+var game = new Game('canvas');
+var map = new MapRayCaster();
+var keydown = false;
+var w = false;
+var s = false;
+var v = 1.5;
+
+game.init = function (context) {
+    context.font = '30px Arial';
+    context.fillStyle = "skyblue";
+    context.strokeStyle = 'blue';
+
+    game.contentRoot = "textures";
+    game.loadTexture("tex1.png");
+};
+
+game.addKeyListener('w', function () {
+    var vx = Math.cos(map.viewerAngle * Math.PI / 180) * v;
+    var vy = Math.sin(map.viewerAngle * Math.PI / 180) * v;
+    if (!map.isBlock(map.viewerX+vx, map.viewerY-vy)) {
+        map.viewerX += vx;
+        map.viewerY -= vy;
+    }
+});
+
+game.addKeyListener('s', function () {
+    var vx = Math.cos(map.viewerAngle * Math.PI / 180) * v;
+    var vy = Math.sin(map.viewerAngle * Math.PI / 180) * v;
+    if (!map.isBlock(map.viewerX - vx, map.viewerY + vy)) {
+        map.viewerX -= vx;
+        map.viewerY += vy;
+    }
+});
+
+game.addKeyListener('a', function () {
+   // map.viewerX -= Math.cos(map.viewerAngle * Math.PI / 180)*v;
+});
+
+game.addKeyListener('d', function () {
+   // map.viewerX += Math.cos(map.viewerAngle * Math.PI / 180) * v;
+});
+
+game.addMouseListener('mousedown', function (e) {
+    var l = game.windowToCanvas(e.clientX, e.clientY);
+    map.viewerX = l.x;
+    map.viewerY = l.y;
+})
+game.addMouseListener('mousemove', function (e) {
+    var l = game.windowToCanvas(e.clientX, e.clientY);
+    map.viewerAngle = -Math.atan((l.y - map.viewerY) / (l.x - map.viewerX)) * 180 / Math.PI;
+    if (l.x < map.viewerX) {
+        map.viewerAngle = 180 + map.viewerAngle;
+    }
+})
+
+game.update = function (time) {
+};
+
+game.render = function (context) {
+    context.strokeStyle = 'black';
+    context.lineWidth = 1;
+    context.strokeRect(0, 0, canvas.width, canvas.height);
+
+    map.draw2D(context);
+    map.draw3D(context);
+};
+
+game.start();
